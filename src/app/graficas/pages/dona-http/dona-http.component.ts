56452b06f371984640b929ce1e3c1e019f6bcedb
Andrés Cifuentes <andrescifuentesdorado@gmail.com>
2021-11-17 11:18:48 -0500

    Proyecto Completo

---
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { Color, Label, MultiDataSet } from 'ng2-charts';
+import { GraficasService } from '../../services/graficas.service';
+
+@Component({
+  selector: 'app-dona-http',
+  templateUrl: './dona-http.component.html',
+  styles: [
+  ]
+})
+export class DonaHttpComponent implements OnInit {
+
+  public doughnutChartLabels: Label[] = [
+    // 'Download Sales', 'In-Store Sales', 'Mail-Order Sales', 'Other'
+  ];
+  public doughnutChartData: MultiDataSet = [
+    // [350, 450, 100, 150],
+    // [50, 150, 120],
+    // [250, 130, 70],
+  ];
+  public doughnutChartType: ChartType = 'doughnut';
+
+  public colors: Color[] = [
+    {
+      backgroundColor: [
+        '#0070FF',
+        '#0CA1E8',
+        '#01EAFF',
+        '#00EBC3',
+        '#00FF93',
+      ]
+    }
+  ];
+
+
+  constructor( private graficasService: GraficasService ) { }
+
+  ngOnInit(): void {
+
+    this.graficasService.getUsuarioRedesSociales()
+         .subscribe (data => {
+          console.log(data);
+
+          const labels = Object.keys( data );
+          const values = Object.values( data );
+          // console.log(labels);
+          // console.log(values);
+
+          this.doughnutChartLabels = labels;
+
+          this.doughnutChartData.push( values );
+    })
+
+  }
+
+}
---
+++ src/app/graficas/pages/dona/dona.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { Color, Label, MultiDataSet } from 'ng2-charts';
+
+@Component({
+  selector: 'app-dona',
+  templateUrl: './dona.component.html',
+  styles: [
+  ]
+})
+export class DonaComponent implements OnInit {
+
+  public doughnutChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales', 'Other'];
+  public doughnutChartData: MultiDataSet = [
+    [350, 450, 100, 150],
+    // [50, 150, 120],
+    // [250, 130, 70],
+  ];
+  public doughnutChartType: ChartType = 'doughnut';
+
+  public colors: Color[] = [
+    {
+      backgroundColor: [
+        '#0070FF',
+        '#0CA1E8',
+        '#01EAFF',
+        '#00EBC3',
+        '#00FF93',
+      ]
+    }
+  ];
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
